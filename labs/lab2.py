ad5d6b873be4ec82c6da7b5b59325b23f2d112f2
Tjelvar Guo <tjegu689@student.liu.se>
2016-02-05 11:50:52 +0100

    added lab2 and lab2db

---
+++ labs/lab2.py
@@ -0,0 +1,94 @@
+import code
+import json
+
+
+from flask import Flask,request,jsonify,abort, current_app, make_response
+from lab2database import User,Messages,db
+app = Flask(__name__)
+msgs = []
+global id_counter, message_list
+message_list = []
+id_counter = 1
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/messages', methods=['GET'])
+def all_msgs():
+    global message_list
+    messages = Messages.query.all()
+    for i in range(len(messages)):
+        if {'id' : messages[i].get_id(), 'message' : messages[i].get_msg()} not in message_list:
+            message_list.append({'id' : messages[i].get_id(), 'message' : messages[i].get_msg()})
+    print(json.dumps(message_list, indent= 2, sort_keys=True))
+    #json.dumps(message_list, indent= 2, sort_keys=True)
+    return json.dumps(message_list, sort_keys=True)#jsonify(Messages = message_list)
+
+
+@app.route('/messages/<MessageID>', methods=['GET'])
+def get_msg(MessageID):
+    for i in message_list:
+        if i['id'] == MessageID:
+            return jsonify(i)
+    abort(400)
+
+
+@app.route('/messages/<MessageID>', methods=['DELETE'])
+def remove_msg(MessageID):
+    global message_list
+
+    remove = Messages.query.filter_by(id=MessageID).delete()
+    update = Messages.query.all()
+    print(update)
+    message_list = []
+    return ""
+
+
+@app.route('/messages/<MessageID>/flag/<UserId>', methods = ['POST'])
+def mark_read(MessageID,UserId):
+    has_marked = False
+    for msg in msgs:
+        if msg['id'] == int(MessageID):
+            msg["readBy"].append(UserId)
+            has_marked = True
+    if not has_marked:
+        abort(400)
+    return ""
+
+
+@app.route('/messages/unread/<UserId>', methods=['GET'])
+def unread_msg(UserId):
+    if not isinstance(str,UserId):
+        abort(400)
+    unreadmsgs = []
+    for i in msgs:
+        if UserId not in i['readBy']:
+            unreadmsgs.append(i)
+    return json.dumps(unreadmsgs)
+
+@app.route('/add_user',methods=['POST'])
+def add_user():
+    user = request.get_json()
+    user = User(user)
+    db.session.add(user)
+    db.session.commit()
+
+    return ""
+
+@app.route('/messages/add', methods=['POST'])
+def add_msg():
+    global id_counter
+    """if(len(msg["message"])>140 and not isinstance(str,msg["message"])):
+        abort(400)"""
+    msg = request.get_json()
+    msg = Messages(msg)
+    db.session.add(msg)
+    db.session.commit()
+    return ""
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(port=9089)
---
+++ labs/lab2database.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/pp.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+
+    def __init__(self, username):
+        self.username = username
+        self.id = id(self)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Messages(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    message = db.Column(db.String(140))
+    def __init__(self,message):
+        self.message = message
+        self.id = id(self)
+
+    def get_msg(self):
+        return self.message
+
+    def get_id(self):
+        return str(self.id)
+
+    def __repr__(self):
+        return '<Message %r>' % self.message + '<Id: %r>' % self.id
